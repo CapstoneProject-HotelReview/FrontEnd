037b23f1b9809878e7362f36baa3443a1fe7dd10
isabelleadrury <isabelledrury38@gmail.com>
2025-12-01 23:52:02 -0500

    WIP: css for rating button

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -53,6 +53,9 @@ export default function review() {
           <h3>Rate this hotel.</h3>
           {[1, 2, 3, 4, 5].map((value) => (
             <span
+              style={{
+                backgroundColor: rating === value ? "black" : "transparent",
+              }}
               key={value}
               data-value={value}
               onClick={() => handleRating(value)}
