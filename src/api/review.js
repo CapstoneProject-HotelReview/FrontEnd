9196b2dbcd6f7f160c8050e6bca3c9a5830f4163
isabelleadrury <isabelledrury38@gmail.com>
2025-12-01 23:39:37 -0500

    deleted bad code.

--- src/api/review.js
+++ src/api/review.js
@@ -1,12 +1,1 @@
 export async function addReview() {}
-
-export async function rating() {
-  const stars = document.querySelectorAll(".rate-btn span");
-  stars.forEach((star) => {
-    star.addEventListener("click", () => {
-      stars.forEach((s) => s.classList.remove("selected"));
-      star.classList.add("selected");
-      console.log("Selected rating:", star.dataset.value);
-    });
-  });
-}
