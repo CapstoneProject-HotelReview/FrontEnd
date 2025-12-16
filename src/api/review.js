c1285a75abe597e3b71053a561b7506bc7a772aa
SHussain47 <hussain.syed.6732@gmail.com>
2025-12-15 20:32:51 -0500

    fix: review.js Api

--- src/api/review.js
+++ src/api/review.js
@@ -1,7 +1,5 @@
 const API = import.meta.env.VITE_API;
 
-export async function addReview() {}
-
 export async function getReviews(userId, page = 1, token) {
   try {
     const response = await fetch(`${API}/reviews/${userId}/reviews?page=${page}`, {
@@ -10,11 +8,31 @@ export async function getReviews(userId, page = 1, token) {
         Authorization: `Bearer ${token}`,
       },
     });
-    if(!response.ok) throw new Error(response.statusText);
+    if (!response.ok) throw new Error(response.statusText);
     const result = await response.json();
     return result;
   } catch (error) {
     console.error("Error with /GET reviews function: ", error);
     return [];
+  }
+}
+
+export async function addReview(hotelId, token, rating, subject, review) {
+  try {
+    const response = await fetch(`${API}/reviews/${hotelId}/review`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ rating, subject, review }),
+    });
+
+    const result = await response.text();
+    if (!response.ok) throw new Error(result);
+    return result;
+  } catch (error) {
+    console.error("Error with /POST hotel reviews", error);
+    throw error;
   }
 }
